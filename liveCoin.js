0cd2d4512a991cef95dc35e716a30d901ae1f688
Bilal Ekmen <92811942+BilalEkmen@users.noreply.github.com>
2023-08-26 00:00:53 +0300

    Update liveCoin.js

--- liveCoin.js
+++ liveCoin.js
@@ -7,3 +7,27 @@ btcData.onmessage = (event) => {
     let priceBtc = parseFloat(stockOpjectBtc.p).toFixed(2)
     console.log(priceBtc)
 }
+
+
+
+const socket = new WebSocket('wss://stream.binance.com:9443/ws/ethusdt@trade');
+
+socket.onopen = () => {
+  console.log('WebSocket connection opened');
+};
+
+socket.onmessage = event => {
+  const data = JSON.parse(event.data);
+  if (data && data.s === 'ETHUSDT') {
+    const price = parseFloat(data.p).toFixed(2);
+    console.log(`Current ETH Price: $${price}`);
+  }
+};
+
+socket.onclose = event => {
+  console.log(`WebSocket connection closed with code: ${event.code}`);
+};
+
+socket.onerror = error => {
+  console.error('WebSocket encountered an error:', error);
+};
